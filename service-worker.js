fcfd8ee9383ba74255015922b2e0bcb0e762e3a6
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-01-23 00:35:14 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const staticDevCoffee = "nukempo"
+const assets = [
+    "/",
+    "/index.html",
+    "/image.png"
+]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(staticDevCoffee).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
